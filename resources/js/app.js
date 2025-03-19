7f0ec2036440f18de3aafa8a0c736d5407dfe083
Jeff <unvedicus@gmail.com>
2025-03-19 21:34:05 +0800

    Updated app.js to remove all used js files and instead place them in vite.config.js to allow individual calls.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,24 +1,3 @@
 import './bootstrap';
 import 'flowbite';
 
-import './apexcharts-global';
-import './crop-combobox';
-import './crop-populate-estimates';
-import './crop-recommendation-grid';
-import './crop-recommendation-tabs';
-import './crop-recommendation-weather';
-import './crop-yield-index';
-import './custom-star-dropdown';
-import './dashboard-index';
-import './map-create';
-import './map-index-controls';
-import './map-index-load';
-import './map-index-populate';
-import './map-rating-show';
-import './map-show';
-import './plot-create';
-import './plot-edit';
-import './plot-show';
-import './soil-index';
-import './toast-notification';
-import './weather';
