0fa3bd7d4065bd1bf37961d0fd56c218c4ea774d
Jeff <unvedicus@gmail.com>
2025-03-19 21:15:48 +0800

    Updated app.js to include all used js files. Removed duplicates in vite.config.js since all of them are now in app.js

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,6 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/js/apexcharts-global.js',
-                'resources/js/weather.js',
-                'resources/js/soil-index.js',
-                'resources/js/map-index-load.js',
             ],
             refresh: true,
         }),
